9de8fdd01da85d96646dc1aa4597bb2dc6cd43ea
sn1047 <sn10479109@gmail.com>
2020-04-29 11:01:48 -0700

    changed the curriculum to use function components, now to get more of its features working - add, edit, move?, archive?, delete?

--- curr-app/src/components/Curriculum/Curriculum.js
+++ curr-app/src/components/Curriculum/Curriculum.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, Redirect} from 'react-router-dom'
 import CategoryInput from 'components/CategoryInput';
 import { Query } from 'react-apollo'
@@ -14,120 +14,86 @@ const INFO_QUERY = gql`
         # info: regularInfo
     }
 `
-class Curriculum extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            topCategories: [],
-            fullCurriculum: [],
-            show: false, 
-            toCurricMain: false
-        }
-    }
 
-    setShow(showModal) {
-        this.setState({show: showModal});
-    }
+function Curriculum(props) {
+    const [show, setShow] = useState(false);
+    const [topCategories, setTopCats] = useState([]);
+    const [toCurricMain, goToMain] = useState(false);
 
-    showModal = (event) => {
+    //query for the topcategories
+    useEffect( () => {
+        const { client, schoolId, defaultArt } = props;
+        console.log("testing")
+        curriculumActions.queryCurriculum(client, schoolId, defaultArt)
+        .then( curriculum => {
+            let topCats = [];
+            if (curriculum) {
+                topCats = curriculum.topCategories;
+            }
+            setTopCats(topCats);
+        });
+    }, [props.schoolId, props.defaultArt] );
+
+    const showModal = (event) => {
         console.log('button:', event.target.name);
         //console.log("cat list", show)
-        this.setShow(true);
+        setShow(true);
     }
 
-    handleOK = (data) => {
-        // console.log("Curric handleOK:", data);
-        this.setShow(false);
+    const handleOK = async (data) => {
+        setShow(false)
     }
 
-    handleCancel = (data) => {
-        // console.log("Curric handleCancel:", data);
-        this.setShow(false);
+    const handleCancel = async (data) => {
+        setShow(false);
     }
 
-    handleCurriculumResponse(curriculum) {
-        let topCategories = [];
-        if (curriculum) {
-            topCategories = curriculum.topCategories;
-        }
-        this.setState( {topCategories} );
-    }
-
-    componentDidMount() {
-        const {schoolId, defaultArt } = this.props;
-        // console.log("s", defaultArt, schoolId);
-        // console.log("client", client)
-        this.GetTopCatsFromQuery(schoolId, defaultArt)
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        const {schoolId, defaultArt } = this.props;
-        if ((prevProps.schoolId !== schoolId && schoolId !== '') || 
-            (prevProps.defaultArt !== defaultArt && defaultArt !== '')) {
-            this.GetTopCatsFromQuery(schoolId, defaultArt)
-        }
-    }
-
-    async GetTopCatsFromQuery(schoolId, art) {
-        const { client } = this.props;
-        const curriculum = await curriculumActions.queryCurriculum(client, schoolId, art)
-        console.log(curriculum)
-        this.handleCurriculumResponse(curriculum);
-    }
-
-    render() {
-        let display = <this.DisplayTopCategories/>;
-        const {show} = this.state;
-        return (
-            <div>
-                <button name='addTopCat' onClick={this.showModal}>
-                    add cat
-                </button>
-                <Query query={INFO_QUERY}>
-                    { ( {loading, error, data} ) => {
-                            if (loading) return <div>Loading Curr</div>
-                            if (error) return <div>Error curr</div>
-                            if (data.info) return <div>{data.info}</div>
-                            return null
-                        }
+    return (
+        <div>
+            <button name='addTopCat' onClick={showModal}>
+                add cat
+            </button>
+            <Query query={INFO_QUERY}>
+                { ( {loading, error, data} ) => {
+                        if (loading) return <div>Loading Curr</div>
+                        if (error) return <div>Error curr</div>
+                        if (data.info) return <div>{data.info}</div>
+                        return null
                     }
-                </Query>
-                
-                <CategoryInput show={show} handleCancel={this.handleCancel} handleOK={this.handleOK}/>
-                {display}
-                <div>be cool to be able to add custom background or image</div>
-                <div>Remove Item</div>
-                <div>Remove Category</div>
-            </div>
-        )
-    }
-    DisplayTopCategories = () => {
-        const {topCategories} = this.state
-        // console.log(topCategories);
-
-        return (
-            <div>
-                <ArtDropdown {...this.props} />
-                <div>Click the Gray Links</div>
-                {topCategories.map(c => {
-                    let link = null;
-                    if (c) {
-                        link =  (
-                            <div key={c._id}>
-                                 {c.title}
-                            </div> 
-                            
-                            // <Link key={c._id} color='White' to={`${this.props.match.url}/${c.title}`}>
-                            //     <div key={c._id} style={{color:'Gray'}}>{c.title}</div>
-                            // </Link>
-                    )}
-                    return link;
-                })}
-                <div>Top Category Only</div>
-                <div>-dives into the category's sub cats/items</div>
-            </div>
-        )
-    }
+                }
+            </Query>
+            <CategoryInput show={show} handleCancel={handleCancel} handleOK={handleOK}/>
+            <DisplayTopCategories {...props} topCategories={topCategories}/>
+            <div>be cool to be able to add custom background or image</div>
+            <div>Remove Item</div>
+            <div>Remove Category</div>
+        </div>
+    )
 }
+
+function DisplayTopCategories({topCategories, ...props}) {
+    return (
+        <div>
+            <ArtDropdown {...props} />
+            <div>Click the Gray Links</div>
+            {topCategories.map(c => {
+                let link = null;
+                if (c) {
+                    link =  (
+                        <div key={c._id}>
+                                {c.title}
+                        </div> 
+                        // <Link key={c._id} color='White' to={`${this.props.match.url}/${c.title}`}>
+                        //     <div key={c._id} style={{color:'Gray'}}>{c.title}</div>
+                        // </Link>
+                )}
+                return link;
+            })}
+            <div>Top Category Only</div>
+            <div>-dives into the category's sub cats/items</div>
+        </div>
+    )
+}
+
 
 export default withApollo(Curriculum)
