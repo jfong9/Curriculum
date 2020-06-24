05db457e03856a4570134bde0b0de9829e91dfcd
sn1047 <sn10479109@gmail.com>
2020-06-24 12:06:57 -0700

    Added front end design for signup page

--- curr-app/src/components/Signup/Signup.js
+++ curr-app/src/components/Signup/Signup.js
@@ -1,75 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { accountsGraphQL } from 'utils/accounts'
-import FormError from 'components/FormError'
-
+import signupStyles from './Signup.module.css'
+import SignUpFields from './SignupFields'
 const LogInLinkButton = props => (
-    <Link to='/login' {...props} >
-        <button>
+    <Link className={signupStyles.link} to='/' {...props} >
             Log In
-        </button>
     </Link>
 );
 
 function Signup( {history} ) {
-
-    const [error, setError] = useState(null);
-    const [user, setUser] = useState({
-        username: '',
-        email: '',
-        password: '',
-    })
-    const handleCreate =  async (e) => {
-        e.preventDefault();
-        try {
-            await accountsGraphQL.createUser({
-                username: user.username,
-                email: user.email,
-                password: user.password
-            })
-            history.push('/login');
-        } catch (err) {
-            setError(err.message)
-        }
-    }
-    const ref = React.createRef();
     return (
-        <form onSubmit={handleCreate}>
-            <label>
-                Userame:
-                <input 
-                    type='text' 
-                    name='username' 
-                    value={user.username} 
-                    onChange={(e) => {setUser({...user, username: e.target.value})}}
-                />
-            </label>
-            <br/>
-            <label>
-                Email:
-                <input 
-                    type='text' 
-                    name='email' 
-                    value={user.email} 
-                    onChange={(e) => {setUser({...user, email: e.target.value})}}
-                />
-            </label>
-            <br/>
-            <label>
-                Password:
-                <input 
-                    type='text' 
-                    name='password' 
-                    value={user.password} 
-                    onChange={(e) => {setUser({...user, password: e.target.value})}}
-                />
-            </label>
-            <br/>
-            <input type='submit' value='create user'/>
-            
-            <LogInLinkButton>Log In1</LogInLinkButton>
-            {error && <FormError error={error}/> }
-        </form>
+        <div className={`${signupStyles.body} ${signupStyles.multiline}`}>
+            <div className={signupStyles.slogan}>
+                {`Get started managing your curriculum today!`}
+            </div>
+            <div className={signupStyles.login}>
+                <SignUpFields history={history}/>
+                <div>
+                    {`\nAlready a curriculum manager? `}
+                    <LogInLinkButton/>
+                </div> 
+            </div>
+
+        </div>
     )
 }
 
